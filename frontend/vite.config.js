753826ca7f04a50fe716c780ef76aca109adfc9f
Julien CAFFET <julien@allianceblock.io>
2024-06-26 22:40:21 +0200

    Add CI/CD

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+  }
 })
