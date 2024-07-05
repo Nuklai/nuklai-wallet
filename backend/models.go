e6fe968ada2355b5eb7fc9dacb8cf4b3eaf27e5e
Piotr Pietruszka <piotr.pietruszka@o2.pl>
2024-07-05 22:45:39 +0200

    feat: help wails to generate correct model types

--- backend/models.go
+++ backend/models.go
@@ -9,27 +9,26 @@ import (
 )
 
 type Alert struct {
-	Type    string
-	Content string
+	Type    string `json:"type"`
+	Content string `json:"content"`
 }
 
 type AddressInfo struct {
-	Name    string
-	Address string
-	AddrStr string
+	Name    string `json:"name"`
+	Address string `json:"address"`
+	AddrStr string `json:"addrStr"`
 }
 
 type TransactionInfo struct {
-	ID        string
-	Size      string
-	Timestamp int64
-	Actor     string
-
-	Success bool
-	Type    string
-	Units   string
-	Fee     string
-	Summary string
+	ID        string `json:"id"`
+	Size      string `json:"size"`
+	Timestamp int64  `json:"timestamp"`
+	Actor     string `json:"actor"`
+	Success   bool   `json:"success"`
+	Type      string `json:"type"`
+	Units     string `json:"units"`
+	Fee       string `json:"fee"`
+	Summary   string `json:"summary"`
 }
 
 type TimeStat struct {
@@ -40,88 +39,78 @@ type TimeStat struct {
 }
 
 type BlockInfo struct {
-	Timestamp int64
-	ID        string
-	Height    uint64
-	Size      string
-	TPS       string
-	Consumed  string
-	Prices    string
-	StateRoot string
-
-	Txs     int
-	FailTxs int
-
-	Latency int64
+	ID        string `json:"id"`
+	Timestamp int64  `json:"timestamp"`
+	Height    uint64 `json:"height"`
+	Size      string `json:"size"`
+	TPS       string `json:"tps"`
+	Consumed  string `json:"consumed"`
+	Prices    string `json:"prices"`
+	StateRoot string `json:"state_root"`
+	Txs       int    `json:"txs"`
+	FailTxs   int    `json:"fail_txs"`
+	Latency   int64  `json:"latency"`
 }
 
 type GenericInfo struct {
-	Timestamp int64
-	Count     uint64
-	Category  string
+	Timestamp int64  `json:"timestamp"`
+	Count     uint64 `json:"count"`
+	Category  string `json:"category"`
 }
 
 type AssetInfo struct {
-	ID string
-
-	Symbol    string
-	Decimals  int
-	Metadata  string
-	Supply    string
-	Creator   string
-	StrSymbol string
+	ID        string `json:"id"`
+	Symbol    string `json:"symbol"`
+	Decimals  int    `json:"decimals"`
+	Metadata  string `json:"metadata"`
+	Supply    string `json:"supply"`
+	Creator   string `json:"creator"`
+	StrSymbol string `json:"strSymbol"`
 }
 
 type BalanceInfo struct {
-	ID string
-
-	Str string
-	Bal string
-	Has bool
+	ID  string `json:"id"`
+	Str string `json:"str"`
+	Bal string `json:"bal"`
+	Has bool   `json:"has"`
 }
 
 type Transactions struct {
-	Alerts  []*Alert
-	TxInfos []*TransactionInfo
+	Alerts  []*Alert           `json:"alerts"`
+	TxInfos []*TransactionInfo `json:"tx_infos"`
 }
 
 type FaucetSearchInfo struct {
-	FaucetAddress string
-	Salt          string
-	Difficulty    uint16
-
-	Solution string
-	Attempts uint64
-	Elapsed  string
-
-	Amount string
-	TxID   string
-
-	Err string
+	FaucetAddress string `json:"faucet_address"`
+	Salt          string `json:"salt"`
+	Difficulty    uint16 `json:"difficulty"`
+	Solution      string `json:"solution"`
+	Attempts      uint64 `json:"attempts"`
+	Elapsed       string `json:"elapsed"`
+	Amount        string `json:"amount"`
+	TxID          string `json:"tx_id"`
+	Err           string `json:"err"`
 }
 
 type FaucetSolutions struct {
-	Alerts        []*Alert
-	CurrentSearch *FaucetSearchInfo
-	PastSearches  []*FaucetSearchInfo
+	Alerts        []*Alert            `json:"alerts"`
+	CurrentSearch *FaucetSearchInfo   `json:"current_search"`
+	PastSearches  []*FaucetSearchInfo `json:"past_searches"`
 }
 
 type FeedInfo struct {
-	Address string
-	Fee     string
+	Address string `json:"address"`
+	Fee     string `json:"fee"`
 }
 
 type FeedObject struct {
-	SubnetID string
-	ChainID  string
-
-	Address   string
-	ID        string
-	Timestamp int64
-	Fee       string
-
-	Message string
-	URL     string
-
-	URLMeta *HTMLMeta
+	ID        string    `json:"id"`
+	SubnetID  string    `json:"subnet_id"`
+	ChainID   string    `json:"chain_id"`
+	Address   string    `json:"address"`
+	Timestamp int64     `json:"timestamp"`
+	Fee       string    `json:"fee"`
+	Message   string    `json:"message"`
+	URL       string    `json:"url"`
+	URLMeta   *HTMLMeta `json:"url_meta"`
 }
